218e3f10ccf4b1eb7d7b68bc9bc376e13de118be
krkchaitanya <krkchaitanya42@gmail.com>
2017-01-30 18:39:46 -0600

    mongoose queries todo

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,37 @@
+const {mongoose}=require('./../server/db/mongoose');
+const {Todo}=require('./../server/models/todo');
+const {ObjectID}=require('mongodb');
+const {User}=require('./../server/models/user');
+
+// var id="588ea4808677b50811449273";
+//
+// if(!ObjectID.isValid(id)){
+//   console.log('id not valid');
+// }
+// Todo.find({
+//   _id:id
+// }).then((todos)=>{
+//   console.log("todos",todos);
+// });
+//
+// Todo.findOne({
+//   _id:id
+// }).then((todo)=>{
+//   console.log("todo",todo);
+// });
+
+// Todo.findById(id).then((todo)=>{
+//   if(!todo){
+//     return console.log("id not found");
+//   }
+//   console.log("todo:",todo);
+// }).catch((e)=>console.log(e));
+
+User.findById("588fcc66191fc0b012a16324").then((user)=>{
+  if(!user){
+    console.log("unable to find the user");
+  }
+  console.log(JSON.stringify(user,undefined,2));
+},(e)=>{
+  console.log(e);
+});
--- server/server.js
+++ server/server.js
@@ -9,6 +9,20 @@ var app = express();
 
 app.use(bodyParser.json());
 
+
+app.post('/users',(req,res)=>{
+var user=new User({
+  email:req.body.email
+});
+user.save().then((doc)=>{
+  res.send(doc);
+},(e)=>{
+  res.status(400).send(e);
+});
+});
+
+
+
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
