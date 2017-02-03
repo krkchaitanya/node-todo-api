55ddce4f24bb374af2d259ceb5977fb9dc5486c7
krkchaitanya <krkchaitanya42@gmail.com>
2017-02-02 20:05:35 -0600

    add get/users token authenticate middleware

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const _=require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate}=require('./middleware/authenticate');
 
 
 var app = express();
@@ -117,6 +118,13 @@ Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
      res.status(400).send(e);
    })
  });
+
+
+
+ app.get('/users/me',authenticate,(req,res)=>{
+res.send(req.user);
+
+});
 
  app.listen(port, () => {
    console.log(`Started up at port ${port}`);
