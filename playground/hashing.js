125d559e71709c2a9e009c62cee2759ae7f36db7
krkchaitanya <krkchaitanya42@gmail.com>
2017-02-03 14:20:41 -0600

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,27 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
-var data={
-  id:10
-};
+var password="123abc";
 
-var token=jwt.sign(data,'abc123');
-console.log(token);
+bcrypt.genSalt(10,(err,salt)=>{
+  bcrypt.hash(password,salt,(err,hash)=>{
+    console.log(hash);
+  })
+});
+
+// var data={
+//   id:10
+// };
 
 
-var decoded=jwt.verify(token,'abc123');
-console.log("decoded",decoded);
+
+// var token=jwt.sign(data,'abc123');
+// console.log(token);
+//
+//
+// var decoded=jwt.verify(token,'abc123');
+// console.log("decoded",decoded);
 // var message="i am user no 3";
 // var hash= SHA256(message).toString();
 //
